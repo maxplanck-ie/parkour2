1300468cdc8fb50086748452bc561eae36792dde
adRn-s <lims@omics.dev>
2022-06-21 09:25:48 +0200

    dj 2.1.15

--- parkour_app/common/urls.py
+++ parkour_app/common/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
-# from django.conf.urls import url  ## quick fix until dj 2.x brings auth blah
 from django.urls import re_path as url
+from django.urls import include, path
 from django.contrib.auth import views as auth_views
 
 from common import views
@@ -12,23 +12,26 @@ urlpatterns = [
     url(
         r"^get_navigation_tree/$", views.get_navigation_tree, name="get_navigation_tree"
     ),
-    url(r"^login/$", auth_views.login, {"template_name": "login.html"}, name="login"),
-    url(r"^logout/$", auth_views.logout, {"next_page": "/"}, name="logout"),
-    url(
-        r"^password_reset/$",
-        auth_views.password_reset,
-        {
-            "post_reset_redirect": "/password_reset/done/",
-            "subject_template_name": "email/password_reset_subject.txt",
-            "email_template_name": "email/password_reset_email.html",
-            "from_email": settings.SERVER_EMAIL,
-        },
+    path(
+        "login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"
+    ),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            success_url="/password_reset/done/",
+            from_email=settings.SERVER_EMAIL,
+            subject_template_name="email/password_reset_subject.txt",
+            email_template_name="email/password_reset_email.html",
+        ),
         name="password_reset",
     ),
-    url(r"^password_reset/done/$", auth_views.password_reset_done),
-    url(
-        r"^password_reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$",
-        auth_views.password_reset_confirm,
-        {"post_reset_redirect": "/accounts/password/done/"},
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view()),
+    path(
+        "password_reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            success_url="/accounts/password/done/"
+        ),
+        name="password_reset_confirm",
     ),
 ]
