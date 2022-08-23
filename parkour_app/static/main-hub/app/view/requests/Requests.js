8400f0e8fdcf32a0919e3f8012d9d4a7100f418c
Nicola Zilio <nicolazilio@gmail.com>
2022-08-23 14:02:24 +0200

    Rename column in Requests from Principal Investigator to PI

--- parkour_app/static/main-hub/app/view/requests/Requests.js
+++ parkour_app/static/main-hub/app/view/requests/Requests.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f1a9e98cc5003bd64f67f75f66d0e6a1619e19dbc4c2a32e749cf70d5c0e724
-size 3803
+oid sha256:c24b2a6bc8a93f9b27f990ac74927640b2ac641a75f5776172847b1b2ceaf554
+size 3811
