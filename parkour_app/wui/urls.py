9f93af4012af19041041c0e9346b7780d8738b3a
Nicola Zilio <nicolazilio@gmail.com>
2022-08-28 17:58:23 +0200

    Remove unnecessary imports from urls

--- parkour_app/wui/urls.py
+++ parkour_app/wui/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.defaults import page_not_found, server_error
 
 from .api import router
 
@@ -17,7 +15,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    import debug_toolbar
 
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls")),
