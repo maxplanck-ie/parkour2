7fdc129cb28863660d92e6d511875dca5145cacf
adRn-s <lims@omics.dev>
2023-08-03 15:54:02 +0200

    WIP: save_demo_data new custom command // also, each json file has the model name without underscores or unmatched pluralizaction

---
+++ parkour_app/common/management/commands/save_demo_data.py
@@ -0,0 +1,43 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Saves relevant database content into fixture(s) correspondingly."
+
+    def handle(self, *args, **options):
+        # Save demo data for all apps
+        self.save_library_sample_shared_fixtures()
+        self.save_sample_fixtures()
+        self.save_index_generator_fixtures()
+        self.save_flowcell_fixtures()
+
+        self.stdout.write(self.style.SUCCESS("Successfully saved demo data."))
+
+    def dumpdata_wrapper(self, model, app_label):
+        with open(app_label + "/fixtures/" + model + ".json", "w") as f:
+            call_command("dumpdata", app_label + "." + model, stdout=f)
+
+    def save_library_sample_shared_fixtures(self):
+        self.dumpdata_wrapper(model="organism", app_label="library_sample_shared")
+        self.dumpdata_wrapper(
+            model="concentrationmethod", app_label="library_sample_shared"
+        )
+        self.dumpdata_wrapper(model="readlength", app_label="library_sample_shared")
+        self.dumpdata_wrapper(model="indexi7", app_label="library_sample_shared")
+        self.dumpdata_wrapper(model="indexi5", app_label="library_sample_shared")
+        self.dumpdata_wrapper(model="indextype", app_label="library_sample_shared")
+        self.dumpdata_wrapper(model="indexpair", app_label="library_sample_shared")
+        self.dumpdata_wrapper(
+            model="libraryprotocol", app_label="library_sample_shared"
+        )
+        self.dumpdata_wrapper(model="librarytype", app_label="library_sample_shared")
+
+    def save_sample_fixtures(self):
+        self.dumpdata_wrapper(model="nucleicacidtype", app_label="sample")
+
+    def save_index_generator_fixtures(self):
+        self.dumpdata_wrapper(model="poolsize", app_label="index_generator")
+
+    def save_flowcell_fixtures(self):
+        self.dumpdata_wrapper(model="sequencer", app_label="flowcell")
