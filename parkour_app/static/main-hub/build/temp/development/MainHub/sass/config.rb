def67684eadbb2224f046dbbb404b234d331f130
Saurabh Dome <domesaurabh@gmail.com>
2023-08-10 15:14:06 +0200

    Changed pytest n to 2

--- parkour_app/static/main-hub/build/temp/development/MainHub/sass/config.rb
+++ parkour_app/static/main-hub/build/temp/development/MainHub/sass/config.rb
@@ -1,2 +1,2 @@
-Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'C:\Users\07StarLord\parkour2\parkour_app\static\main-hub/build/.sass-cache'
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/root/parkour2/parkour_app/static/main-hub/build/.sass-cache'
