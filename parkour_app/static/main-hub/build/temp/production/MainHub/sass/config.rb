7e50fae0dc6f6d9cf8e8f975e56c309d20538c3f
Saurabh Dome <domesaurabh@gmail.com>
2023-08-10 14:31:30 +0200

    Refactored tests directory

--- parkour_app/static/main-hub/build/temp/production/MainHub/sass/config.rb
+++ parkour_app/static/main-hub/build/temp/production/MainHub/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'C:\users\07starlord\parkour2\parkour_app\static\main-hub/build/.sass-cache'
+cache_path = 'C:\Users\07StarLord\parkour2\parkour_app\static\main-hub/build/.sass-cache'
--- parkour_app/static/main-hub/build/temp/production/MainHub/sencha-compiler/app/full-page-master-bundle.js
+++ parkour_app/static/main-hub/build/temp/production/MainHub/sencha-compiler/app/full-page-master-bundle.js
@@ -1,2 +1,2 @@
 // @tag full-page
-// @require C:\users\07starlord\parkour2\parkour_app\static\main-hub\app.js
+// @require C:\Users\07StarLord\parkour2\parkour_app\static\main-hub\app.js
