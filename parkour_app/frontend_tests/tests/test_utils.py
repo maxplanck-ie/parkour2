d882691a713369cbc6f1874fa58983953b7011dc
Saurabh Dome <domesaurabh@gmail.com>
2023-08-11 15:43:30 +0200

    Tests modified for playwright container

--- parkour_app/frontend_tests/tests/test_utils.py
+++ parkour_app/frontend_tests/tests/test_utils.py
@@ -1,15 +1,19 @@
 from playwright.sync_api import Page
 
 testEmailID = "test.user@test.com"
-testPassword = "StrongPassword!1"
+testPassword = "testing.password"
+
+def visit_login_page(page):
+    page.goto("http://127.0.0.1:9980/login")
+    # page.goto("http://0.0.0.0:8000/login")
 
 def pretest_login(page: Page):
     inputEmail = page.locator("input#id_username")
     inputPassword = page.locator("input#id_password")
     loginButton = page.locator("input#login_button")
 
-    # page.goto("http://127.0.0.1:9980/login")
-    page.goto("http://0.0.0.0:8000/login")
+    visit_login_page(page)
+
     inputEmail.fill(testEmailID)
     inputPassword.fill(testPassword)
     loginButton.click()
