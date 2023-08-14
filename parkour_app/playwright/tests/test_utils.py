e085c5749aed4c5691c11c51f103e9dd05fe90a3
adRn-s <salatino@omics.dev>
2023-08-14 11:53:01 +0200

    like this is supposed to work inside docker or CI

--- parkour_app/playwright/tests/test_utils.py
+++ parkour_app/playwright/tests/test_utils.py
@@ -3,9 +3,10 @@ from playwright.sync_api import Page
 testEmailID = "test.user@test.com"
 testPassword = "testing.password"
 
+
 def visit_login_page(page):
-    page.goto("http://127.0.0.1:9980/login")
-    # page.goto("http://0.0.0.0:8000/login")
+    # page.goto("http://127.0.0.1:9980/login")
+    page.goto("http://0.0.0.0:8000/login")
 
 
 def pretest_login(page: Page):
