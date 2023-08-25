13d0e457bdbed0831d4b180a6669a3c976f8c234
Saurabh Dome <dome@pc390.ie-freiburg.mpg.de>
2023-08-25 16:03:18 +0200

    Added tests for forgot password in login page

--- parkour_app/playwright/tests/login_page.py
+++ parkour_app/playwright/tests/login_page.py
@@ -21,24 +21,29 @@ def browser_context_args(browser_context_args):
 def test_login_page(page: Page):
     wrongEmailId = "wrong.email.id@test.com"
     wrongPassword = "wrong.password"
+    forgotPasswordEmailId = "forgot.password.email.id@ie-freiburg.mpg.de"
     correctEmailId = utilities.testEmailID
     correctPassword = utilities.testPassword
+    
     inputEmail = page.locator("input#id_username")
     inputPassword = page.locator("input#id_password")
     loginButton = page.locator("input#login_button")
+    forgotPasswordLink = page.get_by_role("link", name="Forgot password?")
+    forgotPasswordEmailInput = page.get_by_label("Email address:")
+    forgotPasswordEmailSubmitButton = page.get_by_role("button", name="Reset my password")
 
     utilities.visit_login_page(page)
-    expect(page.locator("h2.form-signin-heading")).to_have_text(
-        re.compile(r"Parkour [0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]")
-    )
-
+    expect(page.locator("h2.form-signin-heading")).to_have_text(re.compile(r"Parkour [0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]"))
+    forgotPasswordLink.click()
+    forgotPasswordEmailInput.fill(forgotPasswordEmailId)
+    forgotPasswordEmailSubmitButton.click()
+    expect(page.get_by_text("Password reset sent")).to_be_visible()
+    expect(page.get_by_text("We’ve emailed you instructions for setting your password")).to_be_visible()
+    utilities.visit_login_page(page)
     inputEmail.fill(wrongEmailId)
     inputPassword.fill(wrongPassword)
     loginButton.click()
-    expect(
-        page.get_by_text("Your username and password didn't match. Please try again.")
-    ).to_be_visible()
-
+    expect(page.get_by_text("Your username and password didn't match. Please try again.")).to_be_visible()
     inputEmail.fill(correctEmailId)
     inputPassword.fill(correctPassword)
     loginButton.click()
