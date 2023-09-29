92b8ca6997d822e190607556fc7492f1ea5b3430
Nicola Zilio <nicolazilio@gmail.com>
2023-09-29 11:21:29 +0200

    Edit Hint text in BatchAddWindow

--- parkour_app/static/main-hub/app/view/libraries/BatchAddWindow.js
+++ parkour_app/static/main-hub/app/view/libraries/BatchAddWindow.js
@@ -124,7 +124,7 @@ Ext.define('MainHub.view.libraries.BatchAddWindow', {
           xtype: 'container',
           margin: '0 0 0 30px',
           html: '<span id="edit-hint"><strong>Hint:</strong> To edit multiple cells at once (Excel-like), ' +
-            'select a cell, press Esc, paste data (CTRL + V)</span>'
+            'select a cell and paste data (CTRL + V)</span>'
         },
         // {
         //       text: 'Download RELACS Pellets Abs form',
