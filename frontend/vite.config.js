1077219a8a504874ad369425489049ebd50cca8d
adRn-s <salatino@omics.dev>
2023-11-10 12:09:09 +0100

    yay!!! it works

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
 export default defineConfig({
+  build: {
+    assetsDir: 'vue-assets'
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
