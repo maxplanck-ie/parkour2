b35ad25ce483b086f016a20daa517ea7e5a700b3
Nicola Zilio <nicolazilio@gmail.com>
2023-11-22 17:13:48 +0100

    Force passwords for local users to contain both capital letters and symbols

--- parkour_app/wui/settings/base.py
+++ parkour_app/wui/settings/base.py
@@ -140,12 +140,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 12,
+        },
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+    {
+        'NAME': 'wui.password_validators.CapitalSymbolPasswordValidator',
     },
 ]
 
