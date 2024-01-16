60a4deb7b7c268b123d8c3c47d7a91c55a0aa3f3
adRn-s <lims@omics.dev>
2024-01-16 11:01:17 +0100

    duties app works for dev // tests are still broken, somehow

--- backend/wui/settings/testing.py
+++ backend/wui/settings/testing.py
@@ -1,52 +1,37 @@
 from .base import *
 
-## These are the same as in dev.py but without Django-debug-toolbar
-
 DEBUG = True
 
-# Application definition
-INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "authtools",
-    "rest_framework",
-    "django_admin_listfilter_dropdown",
-    "django_extensions",
-    "import_export",
-    "django_linear_migrations",
-    "common",
-    "library_sample_shared",
-    "library",
-    "sample",
-    "request",
-    "incoming_libraries",
-    "index_generator",
-    "library_preparation",
-    "pooling",
-    "flowcell",
-    "report",
-    "invoicing",
-    "usage",
-    "stats",
-    "metadata_exporter",
-    "drf_spectacular",
+## These are the same as in prod.py
+
+REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+    "rest_framework.renderers.JSONRenderer",
+]
+
+NOTEBOOK_ARGUMENTS += [
+    "--ip",
+    "0.0.0.0",
+    "--allow-root",
+]
+
+## These are the same as in dev.py BUT without Django-debug-toolbar
+
+INSTALLED_APPS += [
+    "schema_viewer",
+    "django_migration_linter",
     "corsheaders",
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE += [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "extra.middleware.ErrorMiddleware",
+]
+
+
+# CORS settings to enable API calls for Vue.js while development
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
 ]
 
 MIGRATION_LINTER_OPTIONS = {
@@ -81,21 +66,3 @@ LOGGING["loggers"] = {
         "handlers": ["rich_console"],
     },
 }
-
-## These are the same as in prod.py
-
-REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
-    "rest_framework.renderers.JSONRenderer",
-]
-
-NOTEBOOK_ARGUMENTS += [
-    "--ip",
-    "0.0.0.0",
-    "--allow-root",
-]
-
-# CORS settings to enable API calls for Vue.js while development
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174",
-]
