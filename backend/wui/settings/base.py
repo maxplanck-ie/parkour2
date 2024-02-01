7a85900f0ef9435c0ab6cce2b92c357d33727bb9
adRn-s <lims@omics.dev>
2024-02-01 14:03:51 +0100

    request.is_secure was returning False because of NGINX -- Fix token_url

--- backend/wui/settings/base.py
+++ backend/wui/settings/base.py
@@ -204,7 +204,7 @@ LOGGING = {
 }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
