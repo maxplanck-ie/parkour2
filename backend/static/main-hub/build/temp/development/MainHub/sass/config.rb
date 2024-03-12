b6f7c1fb8f0e2f6460962ebd6227169d497128cd
Saurabh <domesaurabh@gmail.com>
2024-03-12 16:44:41 +0100

    Delivered status

--- backend/static/main-hub/build/temp/development/MainHub/sass/config.rb
+++ backend/static/main-hub/build/temp/development/MainHub/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/root/parkour2/backend/static/main-hub/build/.sass-cache'
+cache_path = '/docker/parkour2/backend/static/main-hub/build/.sass-cache'
