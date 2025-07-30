f92c30b7ce4fc9fb79ad4f981f91de781a83c2de
Saurabh Dome <domesaurabh@gmail.com>
2025-07-30 02:25:24 +0200

    index field fix

--- backend/sample/migrations/0012_completesampleview.py
+++ backend/sample/migrations/0012_completesampleview.py
@@ -24,15 +24,15 @@ SELECT
     it.name AS index_type_name,
     s.index_i7,
     s.index_i5,
-    (i7.prefix || i7.number) AS i7_id,
-    (i5.prefix || i5.number) AS i5_id,
-    ip.char_coord || ip.num_coord::text AS coordinate,
+    i7.id AS i7_id,
+    i5.id AS i5_id,
+    (ip.char_coord || ip.num_coord) AS coordinate,
     s.read_length_id,
     rl.name AS read_length_name,
     r.id AS request_id,
     r.name AS request_name,
-    ip.name AS pool_name,
-    r.create_time AS create_time
+    r.create_time AS create_time,
+    igp.name AS pool_name
 FROM sample_sample AS s
 JOIN request_request_samples AS rrs ON s.id = rrs.sample_id
 JOIN request_request AS r ON rrs.request_id = r.id
@@ -41,11 +41,13 @@ LEFT JOIN library_sample_shared_libraryprotocol AS lp ON s.library_protocol_id =
 LEFT JOIN library_sample_shared_librarytype AS lt ON s.library_type_id = lt.id
 LEFT JOIN library_sample_shared_indextype AS it ON s.index_type_id = it.id
 LEFT JOIN index_generator_pool_samples AS ips ON s.id = ips.sample_id
-LEFT JOIN index_generator_pool AS ip ON ips.pool_id = ip.id
+LEFT JOIN index_generator_pool AS igp ON ips.pool_id = igp.id
 LEFT JOIN library_preparation_librarypreparation AS lp2 ON lp2.sample_id = s.id
-LEFT JOIN library_sample_shared_indexi7 AS i7 ON s.index_i7 = i7.index
-LEFT JOIN library_sample_shared_indexi5 AS i5 ON s.index_i5 = i5.index
-LEFT JOIN library_sample_shared_readlength AS rl ON s.read_length_id = rl.id;
+LEFT JOIN library_sample_shared_readlength AS rl ON s.read_length_id = rl.id
+LEFT JOIN library_sample_shared_indexi7 AS i7 ON i7.index = s.index_i7
+LEFT JOIN library_sample_shared_indexi5 AS i5 ON i5.index = s.index_i5
+LEFT JOIN library_sample_shared_indexpair AS ip
+    ON ip.index1_id = i7.id AND ip.index2_id = i5.id;
 """
 
 
