30091762e9f6fc48312bbd541a93b6b59b8a69ea
Saurabh Dome <domesaurabh@gmail.com>
2025-11-12 23:06:03 +0100

    migration files

--- backend/library/migrations/0009_refresh_complete_library_data.py
+++ backend/library/migrations/0009_refresh_complete_library_data.py
@@ -75,6 +75,11 @@ class Migration(migrations.Migration):
                         ("sequencing_depth", models.FloatField()),
                         ("measuring_unit", models.CharField(max_length=50)),
                         ("measured_value", models.FloatField()),
+                        (
+                            "measuring_unit_facility",
+                            models.CharField(max_length=50),
+                        ),
+                        ("measured_value_facility", models.FloatField()),
                         ("concentration_library", models.FloatField()),
                         ("percent_total", models.FloatField()),
                         ("library_protocol_id", models.IntegerField()),
