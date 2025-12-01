e63f53555861847651ce97e2b328d0a39e8491b7
adRn-s <salatino@omics.dev>
2025-12-01 13:59:28 +0100

    report_xlsx and a new button there to get this data

--- backend/report/urls.py
+++ backend/report/urls.py
@@ -3,6 +3,7 @@ from report import views
 
 urlpatterns = [
     re_path("report/", views.report, name="report"),
+    re_path("report_xlsx/", views.report_xlsx, name="report-xlsx"),
     re_path("db/", views.database, name="database"),
     re_path("db_data/", views.database_data, name="database-data"),
 ]
